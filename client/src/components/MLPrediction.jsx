3385873a69638ad08d39a52ef852e027d6879894
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2024-03-09 14:29:03 +0530

    project done

---
+++ client/src/components/MLPrediction.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './MLPrediction.css';
+
+const MLPrediction = ({ prediction }) => (
+  <div className="MLPrediction-display">
+    Rakesh: { prediction }
+  </div>
+);
+export default MLPrediction;
